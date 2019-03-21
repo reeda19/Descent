a70485e3124740890174b7a3200f453c1af5b7d4
Alex Reed <alexreed100@gmail.com>
2019-03-21 16:36:55 -0400

    First commit

--- Descent/Descent/Descent.py
+++ Descent/Descent/Descent.py
@@ -1,1 +1,18 @@
+''' ~COMMANDS~
+Help - Displays possible commands
+Get <object> - Picks up object
+Use <object> - Uses object
+Drop <object> - Drops object
+Inventory - Displays all items in your inventory
+Look - Provides description of the room you're in
+Open <Object> - Opens object and reveals its contents
+Inspect <Object> - Inspects object and provides a description
+Talk <Person> - initiates a conversation with a person
+N - Travels to the room that is North
+E - Travels to the room that is East
+S - Travels to the room that is South
+W - Travels to the room that is West
+Compass - Shows available rooms you can travel to'''
 
+while True:
+    response = input('>')
