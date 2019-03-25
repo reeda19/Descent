61bd0e77fa8a83639db440f183a7bccec2aea8e6
zacharytower <zacht7722@gmail.com>
2019-03-25 18:24:38 -0400

    Change syntax on Descent.py

--- Descent/Descent/Descent.py
+++ Descent/Descent/Descent.py
@@ -30,28 +30,33 @@ names = {
     }
 print("Choose a character: Zach, Alex, Jack, Froggy, Olivia:\nType \'describe <character>\' to see a characters description.")
 
-while(True): #character selection
+def main():
+	while(True): #character selection
 
-    name = input('>').lower().strip().split()
-    try:
-        if name[0] in 'describe':
-            print(names[name[1]])
-        elif name[0] in names:
-            print('You have chosen',name[0])
-            protag = Player.Player(name[0])
-            break;
-        else:
-            print('That is not an option, sorry')
-    except IndexError: #no input
-        name = random.choice(list(names.keys()))
-        print('You have chosen',name)
-        protag = Player.Player(name)
-        break;
+	    name = input('>').lower().strip().split()
+	    try:
+		if name[0] in 'describe':
+		    print(names[name[1]])
+		elif name[0] in names:
+		    print('You have chosen',name[0])
+		    protag = Player.Player(name[0])
+		    break
+		else:
+		    print('That is not an option, sorry')
+	    except IndexError: #no input
+		name = random.choice(list(names.keys()))
+		print('You have chosen',name)
+		protag = Player.Player(name)
+		break
 
-while True:
-    response = input('>').lower().strip()
-    #test
-    if response.find('whoami')!=-1:
-        print(protag.name) 
-    if response.find('health')!=-1:
-        print('limbs:',protag.getHealth())
+	while True:
+	    response = input('>').lower().strip()
+	    #test
+	    if 'whoami' in response:
+		print(protag.name)
+	    elif 'health' in response:
+		print('limbs:',protag.getHealth())
+
+if __name__ == '__main__':
+	main()
+
