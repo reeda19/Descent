ba21e04902ce2d7a382ad454577278aff2351711
zacharytower <zacht7722@gmail.com>
2019-03-26 08:26:02 -0400

    Fix camel-casing in Enemy properties

--- Descent/Enemy.py
+++ Descent/Enemy.py
@@ -2,13 +2,14 @@ class Enemy(object):
     """Generic Enemy Template"""
     health = 0
     damage = 0
-    attackName = ''
-    critChance = 0
+    attack_name = ''
+    crit_chance  = 0
     name = ''
-    def __init__(self, health, attackName, critChance, name):
-        self.health=health #if players don't have health, should enemies? 
-        self.attackName=attackName
-        self.critChance=critChance
-        self.name=name
+
+    def __init__(self, health, attack_name, crit_chance , name):
+        self.health = health #if players don't have health, should enemies? 
+        self.attack_name = attack_name
+        self.crit_chance = crit_chance 
+        self.name = name
     def attack(self):
-        return self.critChance
+        return self.crit_chance 
