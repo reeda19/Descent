9a983833a409d2dc0a4599bd864d72ea76318879
zacharytower <zacht7722@gmail.com>
2019-03-26 08:14:16 -0400

    Remove obsolete Descent directory

--- Descent/Descent/Weapon.py
+++
@@ -1,8 +0,0 @@
-class Weapon(Item):
-    """weapon class is subclass of item"""
-    critChance = ''
-    twoHanded = False #One handed or two handed
-    def __init__(self, name, critChance, twoHanded):
-        self.critChance = critChance
-        self.twoHanded = twoHanded
-        super().__init__(False, name) 
