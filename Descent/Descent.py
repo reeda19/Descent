d85811a4375db91e8364ab5c8ab9d9c6b59adc96
Alex Reed <reeda19@hopedaleschools.org>
2019-03-27 12:25:03 +0000

    Added Help command

--- Descent/Descent.py
+++ Descent/Descent.py
@@ -3,7 +3,8 @@ import Enemy
 import Map
 import Location
 import random
-''' ~COMMANDS~  
+HELP_STRING = """
+       ~COMMANDS~  
 Help - Displays possible commands
 Get <object> - Picks up object
 Use <object> - Uses object
@@ -20,7 +21,7 @@ W - Travels to the room that is West
 Compass - Shows available rooms you can travel to
 Attack - attacks an enemy
 Health - displays your current health
-'''
+"""
 RANDOM_BOGUS_STRINGS=['Sorry, that didn\'t make sense', 'I do not understand that', 'That is not an available actions'] #If input is not an action
 direction_string_dict = { #defines which user input leads to which direction
 'n' : 'north',	
@@ -33,11 +34,11 @@ direction_string_dict = { #defines which user input leads to which direction
 'sw' : 'southwest'
 }
 names = {
-    'zach' : 'zach description', #descriptions to be made by olivia
-    'alex' : 'alex description', #Each character has a different storyline/plot
-    'jack' : 'jack description',
-    'froggy' : 'froggy description',
-    'olivia' : 'olivia description'
+    'Zach' : 'zach description', #descriptions to be made by olivia
+    'Alex' : 'alex description', #Each character has a different storyline/plot
+    'Jack' : 'jack description',
+    'Froggy' : 'froggy description',
+    'Olivia' : 'olivia description'
     }
 print("Choose a character: Zach, Alex, Jack, Froggy, Olivia:\nType \'describe <character>\' to see a character\'s description.")
 
@@ -47,7 +48,7 @@ def main():
         try:
             if name[0] in 'describe':
                 print(names[name[1]])
-            elif name[0] in names:
+            elif name[0] in [x.lower() for x in list(names.keys())]:
                 print('You have chosen',name[0])
                 protag = Player.Player(name[0])
                 break
@@ -66,11 +67,13 @@ def main():
         keywords = user_input.split()
         if len(keywords) == 0: #prevents index out of range error
             continue
-        elif user_input.find('whoami')!=-1:
+        elif keywords[0] in ['help']:
+            print(HELP_STRING)
+        elif keywords[0] in ['whoami']:
             print(protag.name) 
-        elif user_input.find('health')!=-1:
+        elif keywords[0] in ['health']:
             print(protag.getHealth())
-        elif keywords[0] in 'look':
+        elif keywords[0] in ['look']:
             print(protag.location.description)
         elif keywords[0] in 'nesw senw':
             #move player in that direction
