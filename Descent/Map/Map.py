a18e6ae98039e46656c4e3e0b06897cec768bf77
Alex Reed <reeda19@hopedaleschools.org>
2019-04-10 15:17:44 +0000

    Added call_rop function to be called by Map.py when ROP occurs

--- Descent/Map/Map.py
+++ Descent/Map/Map.py
@@ -1,14 +1,14 @@
 import Player
 import GameMap
 import GameROP
-
+from Descent import protag
 class Map(object):
 	"""
 	MAP object source.
 
 	Map objects store and manage all data about the current game state.
 	Players in the game are defined locally to it, as well as
-	all instances of locations, including how they nodally connect to
+	all instances of locations, including how they nodully connect to
 	one another.
 
 	Map is able to define rites of passages (i.e. in-game hinderances
@@ -20,12 +20,11 @@ class Map(object):
 	"""
 
 	def __init__(self):
-		self.protagonist = Player.Player('place_holder')
 		self.locations = GameMap.all_locations #Defines every location in a list
 		self.rites_of_passage = GameROP.all_rops #Defines every rite of passage in a list
 
 	def move_player(self, new_location):
-		if any(rop.trigger_rop(protagonist.location, new_location)
-			for rop in self.rites_of_passage):
+		for rop in self.rites_of_passage:
+			if any(rop.trigger_rop(protag.location, new_location)):
 			# this location requires us to trigger this rop
-			rop.
+				rop.call_rop()
--- Descent/Rites Of Passage/RiteOfPassage.py
+++ Descent/Rites Of Passage/RiteOfPassage.py
@@ -15,23 +15,33 @@ class RiteOfPassage:
 
 	The passage function defines what behavior should be exhibited
 	when the rite of passage is invoked.
+	
+	The key boolean states whether a player has completed the passage_function or not.
+	Defaults to false until the passage_function is completed.
 	'''
 	description = ''
 	
 	def __init__(self, location_from,
 	 location_to, 
 	 passage_function = null_function,
-	 description = ''
+	 description = '',
+	 key = False
 	 ):
 		self.location_from = location_from
 		self.location_to = location_to
 		self.passage_function = passage_function
-
+		self.key=key
+		
 	def __call__(self, Map = ''):
-		return self.passage_function(self.Map)
+		return self.passage_function(Map)
 
 	def __str__(self):
 		return self.description
 
+		#returns true if parameters are the same as ROP's location
 	def trigger_rop(self, location_from, location_to):
 		return self.location_from == location_from and self.location_to == location_to
+	
+		
+	def call_rop(self): #called by Map.py when a player attempts to move in a direction that contains a ROP
+		#start ROP
